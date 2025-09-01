84136a9047b53c05931b78cf910cf29fa549f1cc
Andru-86 <super.jin2001@yandex.ru>
2025-09-01 18:16:22 +0500

    Add files via upload

---
+++ LargeLayout/js/main.js
@@ -0,0 +1,31 @@
+(function () {
+
+
+    document.addEventListener('click', burgerInit)
+
+    function burgerInit(e) {
+
+        const burgerIcon = e.target.closest('.burger-btn')
+        const burgerNavLink = e.target.closest('.header__nav-list-item-link')
+
+        console.log(document.documentElement.clientWidth);
+        
+
+        if (!burgerIcon && !burgerNavLink) return
+
+        if (document.documentElement.clientWidth > 1111) return
+
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+
+
+
+
+
+    }
+})()
+
+
